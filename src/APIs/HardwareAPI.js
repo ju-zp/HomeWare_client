833a27c505e0f8876fb50086b4794337dee03cb4
ju-zp <jhuntington@tutanota.com>
2019-01-14 08:57:23 +0000

    sorted out indentation

--- src/APIs/HardwareAPI.js
+++ src/APIs/HardwareAPI.js
@@ -1,27 +1,27 @@
 class HardWareAPI {
-     static init(){
-         this.baseURL = "http://127.0.0.1:3002"
-         this.switchOnURL = this.baseURL + '/switchOn'
-         this.switchOffURL = this.baseURL + '/switchOff'
-         this.setColorURL = this.baseURL + '/setColor'
-         this.tempReadingURL = this.baseURL + '/temperature'
-     }
+    static init(){
+        this.baseURL = "http://127.0.0.1:3002"
+        this.switchOnURL = this.baseURL + '/switchOn'
+        this.switchOffURL = this.baseURL + '/switchOff'
+        this.setColorURL = this.baseURL + '/setColor'
+        this.tempReadingURL = this.baseURL + '/temperature'
+    }
 
-     static switchOn(){
-         fetch(this.switchOnURL)
-     }
+    static switchOn(){
+        fetch(this.switchOnURL)
+    }
 
-     static switchOff(){
-         fetch(this.switchOffURL)
-     }
+    static switchOff(){
+        fetch(this.switchOffURL)
+    }
 
-     static setColor(color){
-         fetch(this.setColorURL, {
+    static setColor(color){
+        fetch(this.setColorURL, {
             method: "PATCH",
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify(color) 
-         })
-     }
+        })
+    }
 }
 
 HardWareAPI.init()
