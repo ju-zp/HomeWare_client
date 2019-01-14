3ab9500b9875caca5ee9ace98cb560fb0b5cee47
ju-zp <jhuntington@tutanota.com>
2019-01-14 14:46:32 +0000

    fixed typo on welcome function

--- src/APIs/HardwareAPI.js
+++ src/APIs/HardwareAPI.js
@@ -8,9 +8,9 @@ class HardWareAPI {
         this.tempReadingURL = this.baseURL + '/temperature'
     }
 
-    static welcome({
+    static welcome(){
         fetch(this.welcomeURL)
-    })
+    }
 
     static switchOn(){
         fetch(this.switchOnURL)
