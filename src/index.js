2a600666dd6d5ef694341570f76dd8d33bca7860
ju-zp <jhuntington@tutanota.com>
2019-01-15 11:21:16 +0000

    updated imports to require redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router} from 'react-router-dom'
+
 
 ReactDOM.render(
     <Router>
