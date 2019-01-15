91a6af6908bcdbeaa949d6d3362d5a2b8ed58d9a
ju-zp <jhuntington@tutanota.com>
2019-01-15 11:00:36 +0000

    added handleChange function to handle color select change

--- src/scenes/controller/components/ColorSettings.js
+++ src/scenes/controller/components/ColorSettings.js
@@ -18,32 +18,37 @@ const styles = () => ({
 })
 
 class ControllerSetting extends Component {
+
+    handleChange = () => {
+        console.log('hello')
+    }
+
     render(){
-    const { classes, handleSlider, handleSave, showSave, save, colors } = this.props
-    return <div>
-        <Typography className={classes.title}>
-            Color Settings
-        </Typography>
-        {colors.length > 0 
-            ? <Select value="color">
-                <MenuItem value="">--None--</MenuItem>
-                {colors.map(c => <MenuItem key={c.id} value={c.name}>{c.name}</MenuItem>)}
-            </Select>
-            : <h1>no colors</h1>}
-        <Typography className={classes.text}>
-            Red
-        </Typography>
-        <ColorSlider handleSlider={handleSlider} color="red"/>
-        <Typography className={classes.text}>
-            Blue
-        </Typography>
-        <ColorSlider handleSlider={handleSlider} color="blue"/>
-        <Typography className={classes.text}>
-            Green
-        </Typography>
-        <ColorSlider handleSlider={handleSlider} color="green"/>
-        {showSave ? <SaveForm save={save}/>: <Button onClick={handleSave}>Save</Button>}
-    </div>
+        const { classes, handleSlider, handleSave, showSave, save, colors } = this.props
+        return <div>
+            <Typography className={classes.title}>
+                Color Settings
+            </Typography>
+            {colors.length > 0 
+                ? <Select value="color">
+                    <MenuItem value="">--None--</MenuItem>
+                    {colors.map(c => <MenuItem key={c.id} value={c.name}>{c.name}</MenuItem>)}
+                </Select>
+                : <h1>no colors</h1>}
+            <Typography className={classes.text}>
+                Red
+            </Typography>
+            <ColorSlider handleSlider={handleSlider} color="red"/>
+            <Typography className={classes.text}>
+                Blue
+            </Typography>
+            <ColorSlider handleSlider={handleSlider} color="blue"/>
+            <Typography className={classes.text}>
+                Green
+            </Typography>
+            <ColorSlider handleSlider={handleSlider} color="green"/>
+            {showSave ? <SaveForm save={save}/>: <Button onClick={handleSave}>Save</Button>}
+        </div>
     }
 }
 
