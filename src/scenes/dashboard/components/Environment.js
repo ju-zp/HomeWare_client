07cdbdccc7dd21ba90d4ec457b148e40dd951d67
ju-zp <jhuntington@tutanota.com>
2019-01-16 15:22:17 +0000

    using state to set titles in environment component;

--- src/scenes/dashboard/components/Environment.js
+++ src/scenes/dashboard/components/Environment.js
@@ -8,8 +8,11 @@ class Environment extends Component {
     render(){
         console.log(this.props)
         return <div className='environment'>  
-            <h1>Environment</h1>
-            <h1>wow</h1>
+            <h1>{this.props.home}</h1>
+            <h1>Connected devices:</h1>
+            <ul>
+                
+            </ul>
         </div>
     }
 }
