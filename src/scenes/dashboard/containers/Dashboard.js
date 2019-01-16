56d238889a49685014a05dc98d8a698656064533
ju-zp <jhuntington@tutanota.com>
2019-01-16 14:05:18 +0000

    added components folder and first component

--- src/scenes/dashboard/containers/Dashboard.js
+++ src/scenes/dashboard/containers/Dashboard.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 
 class Dashboard extends Component {
 
-    componentDidMount(){
-        // this.props.history.push('/dashboard')
-    }
-
     render(){
         return <h1>Dashboard</h1>
     }
