df4d8d57cff053e3b7c95b19fe88527d644f78f9
ju-zp <jhuntington@tutanota.com>
2019-01-17 14:57:35 +0000

    moved graphs into tabs

--- src/scenes/dashboard/containers/Temperature.js
+++ src/scenes/dashboard/containers/Temperature.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
+import { Tabs, Tab } from '@material-ui/core'
+
 import API from '../../../APIs/API'
 import SmallGraph from '../components/SmallGraph';
+
 
 class Temperature extends Component {
 
     state = {
-        data: []
+        data: [],
+        value: 0
     }
 
     componentDidMount() {
@@ -14,11 +18,22 @@ class Temperature extends Component {
             .then(data => this.setState({data: data.data}))
     }
 
+    onChange = (e, value) => {
+        this.setState({value})
+    }
+
     render(){
         console.log(this.state)
+        const { data, value } = this.state
+        const { onChange } = this
         return <div className='temperatureContainer'>
             <h3>Temperature:</h3>
-            <SmallGraph data={this.state.data}/>
+            <Tabs value={value} onChange={onChange}>
+                <Tab label='Last 15 mins'></Tab>
+                <Tab label='Last hour'></Tab>
+            </Tabs>
+            {value === 0 &&  <SmallGraph data={data}/>}
+           
         </div>
     }
 }
