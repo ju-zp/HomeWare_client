a122150285b11b2733b5b84a9478119d40c2a59a
ju-zp <jhuntington@tutanota.com>
2019-01-17 15:09:33 +0000

    added large graph to show data for the last hour

--- src/scenes/dashboard/containers/Temperature.js
+++ src/scenes/dashboard/containers/Temperature.js
@@ -4,7 +4,7 @@ import { Tabs, Tab } from '@material-ui/core'
 
 import API from '../../../APIs/API'
 import SmallGraph from '../components/SmallGraph';
-
+import LargeGraph from '../components/LargeGraph'
 
 class Temperature extends Component {
 
@@ -23,16 +23,17 @@ class Temperature extends Component {
     }
 
     render(){
-        console.log(this.state)
         const { data, value } = this.state
         const { onChange } = this
+        const short = data.slice(data.length-30, data.length)
         return <div className='temperatureContainer'>
             <h3>Temperature:</h3>
             <Tabs value={value} onChange={onChange}>
                 <Tab label='Last 15 mins'></Tab>
                 <Tab label='Last hour'></Tab>
             </Tabs>
-            {value === 0 &&  <SmallGraph data={data}/>}
+            {value === 0 &&  <SmallGraph data={short}/>}
+            {value === 1 && <LargeGraph data={data}/>}
            
         </div>
     }
