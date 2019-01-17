5e60fd8bf2f76a0b4a3ef794290fbaa82938e81a
ju-zp <jhuntington@tutanota.com>
2019-01-17 11:24:52 +0000

    moved boardTabs to their own class

--- src/scenes/dashboard/components/Environment.js
+++ src/scenes/dashboard/components/Environment.js
@@ -1,56 +1,17 @@
 import React, { Component } from 'react'
 
-import { Tabs, Tab } from '@material-ui/core'
+import BoardTabs from './BoardTabs'
 
-import API from '../../../APIs/API'
 
 
 class Environment extends Component {
 
-    constructor(props){
-        super()
-        // console.log(props.boards)
-    }
-
-    state = {
-        value: 0
-    }
-
-    handleChange = (e, value) => {
-        this.setState({value})
-    }
-
-    renderBoardInfo = value => {
-        const lights = this.props.boards[value].lights
-        const temperatures = this.props.boards[value].temperatures
-        return <div>
-            <h3>Lights ({lights.length}): </h3>
-            <ol>
-                {lights.map(l => <li>Status:{l.switched_on? " on":" off"}</li>)}
-            </ol>
-            <h3>Tmperature Senors ({temperatures.length}):</h3>
-            <ol>
-                {temperatures.map(t => <li>Interval: {t.interval} seconds</li>)}
-            </ol>
-        </div>
-    }
-
     render(){
-        const { handleChange } = this
-        const { value } = this.state
-        let count = 0
+    
         return <div className='environment'>  
             <h1>{this.props.home}</h1>
             <h1>Connected devices:</h1>
-            <Tabs value={value} onChange={handleChange}>
-            {this.props.boards.map(board => <Tab label={board.name}/>)}
-            </Tabs>
-            {this.props.boards[0] ? 
-            value === count && this.props.boards.map(b => {
-                count++
-                return this.renderBoardInfo(value)
-                })
-            : null}
+            <BoardTabs boards={this.props.boards}/>
         </div>
     }
 }
