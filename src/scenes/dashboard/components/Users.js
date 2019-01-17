68db04f52d4196ff4aaa7f895d71bb8a4d41c037
ju-zp <jhuntington@tutanota.com>
2019-01-17 12:01:26 +0000

    moved call to api into environment component from dashboard

--- src/scenes/dashboard/components/Users.js
+++ src/scenes/dashboard/components/Users.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 class Users extends Component{
+
+    componentDidMount() {
+        
+    }
     render(){
         return <div className='usersContainer'>
             <h3>Users</h3>
--- src/scenes/dashboard/containers/Environment.js
+++ src/scenes/dashboard/containers/Environment.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import BoardTabs from '../components/BoardTabs'
 import Users from '../components/Users'
+import { setHome, setBoards} from '../../../actions/actions'
+import API from '../../../APIs/API'
 
 
 
 class Environment extends Component {
 
+    componentDidMount() {
+        API.getEnvironment(localStorage.username)
+            .then(data => {
+                this.props.setHome(data.name)
+                this.props.setBoards(data.boards)
+            })
+    }
+
     render(){
         return <div className='environment'>  
-            <h1>{this.props.home}</h1>
+            <h1 className='homeTitle'>{this.props.home}</h1>
             <Users/>
             <BoardTabs boards={this.props.boards}/>
         </div>
     }
 }
 
-export default Environment
+
+const mapStateToProps = state => {
+    return{
+        home: state.home,
+        boards: state.board
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return { 
+        setHome: home => dispatch(setHome(home)),
+        setBoards: boards => dispatch(setBoards(boards))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Environment)
