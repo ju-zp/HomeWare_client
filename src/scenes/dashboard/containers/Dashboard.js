0f7ac73ede1350ebc5b468187de2a73939ad1908
ju-zp <jhuntington@tutanota.com>
2019-01-22 14:53:40 +0000

    updated css on temperature

--- src/scenes/dashboard/containers/Dashboard.js
+++ src/scenes/dashboard/containers/Dashboard.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 
-
+import { Typography, withStyles} from '@material-ui/core'
 
 import Environment from './Environment'
 import Temperature from './Temperature'
 import Weather from './Weather'
 
+const styles = {
+    title: {
+        fontSize: '35px',
+        color: 'white',
+        marginLeft: '5%',
+        marginTop: '1%'
+    }
+}
+
 
 class Dashboard extends Component {
 
     render(){
+        const { classes } = this.props
         return <div>
+            <Typography className={classes.title}>
+                Dashboard
+            </Typography>
             <Environment/>
             <Temperature/>
             <Weather/>
@@ -22,4 +35,4 @@ class Dashboard extends Component {
 
 
 
-export default Dashboard
+export default withStyles(styles)(Dashboard)
--- src/scenes/dashboard/containers/Temperature.js
+++ src/scenes/dashboard/containers/Temperature.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
-import { Tabs, Tab } from '@material-ui/core'
+import { Tabs, Tab, Typography, withStyles } from '@material-ui/core'
 
 import SmallGraph from '../components/SmallGraph';
 import LargeGraph from '../components/LargeGraph'
+
+const styles = {
+    title: {
+        fontSize: '25px',
+        color: '#004857',
+        marginLeft: '5%',
+        marginTop: '2%'
+    },
+    list: {
+        fontSize: '15px',
+        color: '#004857'
+    }
+}
 
 class Temperature extends Component {
 
@@ -18,17 +31,25 @@ class Temperature extends Component {
     render(){
         const { value } = this.state
         const { onChange } = this
+        const { classes } = this.props
         return <div className='temperatureContainer'>
-            <h3>Temperature</h3>
-            <Tabs value={value} onChange={onChange}>
-                <Tab label='Last 15 mins'></Tab>
-                <Tab label='Last hour'></Tab>
+            <Typography className={classes.title}>
+                Temperature
+            </Typography>
+            <Tabs value={value} 
+                onChange={onChange}
+                >
+                <Tab className={classes.list} 
+                    label='Last 15 mins'>
+                </Tab>
+                <Tab className={classes.list} 
+                    label='Last hour'>
+                </Tab>
             </Tabs>
             {value === 0 &&  <SmallGraph />}
             {value === 1 && <LargeGraph />}
-           
         </div>
     }
 }
 
-export default Temperature
+export default withStyles(styles)(Temperature)
